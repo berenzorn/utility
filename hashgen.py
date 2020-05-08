c392927ba9a5edf9cbdff69b3cc742d73a046448
berenzorn <berenzorn@gmail.com>
2020-05-08 15:55:42 +0300

    new del, no fullsync

--- hashgen.py
+++ hashgen.py
@@ -85,35 +85,35 @@ if __name__ == '__main__':
     Append mode +
     Modified files in the source compared to the destination - put into copy list.
     Files in the destination without hash - put file in the source into copy list.
-    3 - full sync mode. Erase hashes in source, calc again.
-    Sync mode +
-    Delete files in the destination that are not in the source.
     """
 
     parser = argparse.ArgumentParser()
     parser.add_argument("source")
     parser.add_argument("destination")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-a", "--append", action="store_true", help="Hash-copy new files only. Default.")
-    group.add_argument("-s", "--sync", action="store_true", help="Hash-copy new and changed files")
-    group.add_argument("-f", "--fullsync", action="store_true", help="Full sync source and destination")
-    group.add_argument("-nc", "--nocopy", action="store_true", help="No sync, just hash source files")
+    group.add_argument("-a", "--append", action="store_true", help="Hash & copy new files only. Default")
+    group.add_argument("-s", "--sync", action="store_true", help="Hash & copy new and changed files")
+    parser.add_argument("-d", "--delete", action="store_true", help="Sync & delete old files in destination")
+    parser.add_argument("-nc", "--nocopy", action="store_true", help="No copy, just hash source files")
     parser.add_argument("-q", "--quiet", action="store_true", help="Quiet mode")
-    parser.add_argument("-l", "--log", action="store_true", help="Write output to log file")
     parser.add_argument("-b", type=int, dest="buffer", metavar=" BUFFER", help="Buffer size in MB", default=100)
+    parser.add_argument("-l", type=str, dest="log", metavar=" LOG", help="Write output to log file")
     args = parser.parse_args()
     if args.log:
-        logging.basicConfig(filename="hashgen.log", filemode='a', format='%(asctime)s %(message)s',
+        logging.basicConfig(filename=args.log, filemode='a', format='%(asctime)s %(message)s',
                             datefmt='%m.%d.%Y %H:%M:%S', level=logging.DEBUG)
     if args.buffer <= 0:
         args.buffer = 1
     buffer_size = args.buffer * 1024**2
 
-    if not args.sync and not args.fullsync:
+    if not args.sync:
         args.append = True
 
     if args.log:
-        logging.debug("-=-=-=-=-=-=-=-=-")
+        logging.debug("-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-")
+        logging.debug(f"args.source={args.source}, args.destination={args.destination}")
+        logging.debug(f"args.append={args.append}, args.sync={args.sync}, args.nocopy={args.nocopy}")
+        logging.debug(f"args.delete={args.delete}, args.quiet={args.quiet}, args.buffer={args.buffer}")
 
     if(Path(args.source).exists()) and (Path(args.destination).exists()):
         # 0 - files in src and dst, 1 - new in src, 2 - not in src
@@ -138,32 +138,32 @@ if __name__ == '__main__':
                     logging.debug(f"{i}")
 
     for_copy = []
-    if args.sync or args.fullsync:
+    if args.sync:
         if source_list[2]:
             if not args.quiet:
                 print("\nFiles that are not in the source: ")
                 for x in source_list[2]:
                     print(x)
-                if args.fullsync:
+                if args.delete:
                     print("\nRemoving these files...")
             if args.log:
                 logging.debug("")
                 logging.debug("Files that are not in the source:")
                 for x in source_list[2]:
                     logging.debug(f"{x}")
-                if args.fullsync:
+                if args.delete:
                     logging.debug("")
                     logging.debug("Removing these files...")
 
             for x in source_list[2]:
-                if args.fullsync:
+                if args.delete:
                     try:
                         os.remove(Path(f"{args.destination}\\{x}"))
                         os.remove(Path(f"{args.destination}\\{x}.sha1"))
                     except FileNotFoundError:
                         pass
                 source_list[0].remove(x)
-    if args.sync or args.fullsync:
+    if args.sync:
         if not args.quiet:
             print()
         if args.log:
@@ -174,16 +174,18 @@ if __name__ == '__main__':
             except FileNotFoundError:
                 pass
             sha1_write(args.source, x, buffer_size, args.quiet, args.log)
+
         dest_list = exist_files_check(args.source, args.destination, source_list[0],
                                       buffer_size, args.quiet, args.log)
         if dest_list:
             for x in dest_list:
                 for_copy.append(x)
 
-    if args.append and not args.quiet:
-        print()
-    if args.append and args.log:
-        logging.debug("")
+    if args.append:
+        if not args.quiet:
+            print()
+        if args.log:
+            logging.debug("")
     if args.nocopy:
         # this one + [1] -> all files in source
         for x in sorted(list(set(source_list[0]) - set(source_list[2]))):
